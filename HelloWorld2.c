744bcdee657990983ecdd68f988a179e2f928f2a
vishalkumargourav <vishal.vs911@gmail.com>
2016-01-20 23:20:37 +0530

    Create HelloWorld2.c

---
+++ HelloWorld2.c
@@ -0,0 +1,12 @@
+#include<linux/module.h>
+#include<linux/init.h>
+static int __init hello_init(void){
+	printk(KERN_ALERT "\nTEST:This is intialization function...\n");
+	return 0;
+}
+static void __exit hello_exit(void){
+	printk(KERN_ALERT "\nTEST:This is termination function\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
+MODULE_LICENSE("GPL");
